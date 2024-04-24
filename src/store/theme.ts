b46e3e676e1a7496aa33b654e28e30ce12bbcf49
Mustafa Alshammaa <adam.benals@gmail.com>
2024-04-24 10:17:49 +0200

    fix deprecated code

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,60 +1,60 @@
-import { persistentAtom } from '@nanostores/persistent';
-import { onMount } from 'nanostores';
+import { persistentAtom } from '@nanostores/persistent'
+import { onMount } from 'nanostores'
 
-import { changeGiscusTheme } from '@/components/giscus';
+import { changeGiscusTheme } from '@/components/giscus'
 
 export const THEME_MAP = {
   light: 'light',
   dark: 'dark',
   system: undefined,
-} as const;
+} as const
 
-export type ThemeKey = keyof typeof THEME_MAP;
-export type ThemeValue = (typeof THEME_MAP)[ThemeKey];
+export type ThemeKey = keyof typeof THEME_MAP
+export type ThemeValue = (typeof THEME_MAP)[ThemeKey]
 
-export const STORAGE_THEME_KEY = 'theme' as const;
+export const STORAGE_THEME_KEY = 'theme' as const
 
 export const themeStore = persistentAtom<ThemeValue>(
   STORAGE_THEME_KEY,
   THEME_MAP.system
-);
+)
 
 const initThemeStoreSubscribe = () => {
   const applyTheme = (theme: ThemeValue) => {
     if (theme === THEME_MAP.dark) {
-      changeGiscusTheme('dark');
-      document.documentElement.classList.add('dark');
+      changeGiscusTheme('dark')
+      document.documentElement.classList.add('dark')
     } else if (theme === THEME_MAP.light) {
-      changeGiscusTheme('light');
-      document.documentElement.classList.remove('dark');
+      changeGiscusTheme('light')
+      document.documentElement.classList.remove('dark')
     }
-  };
+  }
 
   const handleMediaQuery = (query: { matches: boolean }) => {
-    applyTheme(query.matches ? THEME_MAP.dark : THEME_MAP.light);
-  };
+    applyTheme(query.matches ? THEME_MAP.dark : THEME_MAP.light)
+  }
 
   themeStore.subscribe((theme) => {
     if (theme !== THEME_MAP.system) {
-      applyTheme(theme);
-      return;
+      applyTheme(theme)
+      return
     }
 
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
     if (!!mediaQuery.addEventListener) {
-      mediaQuery.removeEventListener('change', handleMediaQuery);
-      mediaQuery.addEventListener('change', handleMediaQuery);
+      mediaQuery.removeEventListener('change', handleMediaQuery)
+      mediaQuery.addEventListener('change', handleMediaQuery)
     } else {
-      mediaQuery.removeListener(handleMediaQuery);
-      mediaQuery.addListener(handleMediaQuery);
+      mediaQuery.removeEventListener('change', handleMediaQuery)
+      mediaQuery.addEventListener('change', handleMediaQuery)
     }
-    handleMediaQuery(mediaQuery);
-  });
-};
+    handleMediaQuery(mediaQuery)
+  })
+}
 
 if (typeof window !== 'undefined') {
   onMount(themeStore, () => {
-    initThemeStoreSubscribe();
-  });
+    initThemeStoreSubscribe()
+  })
 }
