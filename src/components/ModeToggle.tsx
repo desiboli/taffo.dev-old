423a384dda29e83140a9b182b45495ef5f14665a
Mustafa Alshammaa <adam.benals@gmail.com>
2024-04-24 09:38:11 +0200

    trying to fix dropdown menu bug in ios

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,26 +1,22 @@
-import { useStore } from '@nanostores/react';
+import { useStore } from '@nanostores/react'
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { DotIcon, MoonIcon, SunIcon } from 'lucide-react';
-import { THEME_MAP, type ThemeKey, themeStore } from '@/store/theme';
+} from '@/components/ui/dropdown-menu'
+import { DotIcon, MoonIcon, SunIcon } from 'lucide-react'
+import { THEME_MAP, type ThemeKey, themeStore } from '@/store/theme'
 
 export function ModeToggle() {
-  const theme = useStore(themeStore);
+  const theme = useStore(themeStore)
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="translate-y-0 translate-x-0"
-        >
+        <Button variant="ghost" size="icon">
           <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -28,7 +24,7 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         {Object.keys(THEME_MAP).map((key) => {
-          const themeKey = key as ThemeKey;
+          const themeKey = key as ThemeKey
           return (
             <DropdownMenuItem
               key={key}
@@ -38,9 +34,9 @@ export function ModeToggle() {
               {themeKey}
               {theme === THEME_MAP[themeKey] && <DotIcon />}
             </DropdownMenuItem>
-          );
+          )
         })}
       </DropdownMenuContent>
     </DropdownMenu>
-  );
+  )
 }
