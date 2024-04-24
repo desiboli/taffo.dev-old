27a57026ae5d394274e3452c0f3566f8da8e1c98
Mustafa Alshammaa <adam.benals@gmail.com>
2024-04-24 09:52:09 +0200

    trying to fix dropdown menu bug in ios

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -14,7 +14,7 @@ export function ModeToggle() {
   const theme = useStore(themeStore)
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
           <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
